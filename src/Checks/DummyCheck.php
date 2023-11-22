8d1e989e8b639cedf3f76b658610291fe5114fcf
ValeriyaKorshikova <korshikova@mindtwo.de>
2023-11-22 15:04:34 +0100

    WIP:add Checks&Test

--- src/Checks/DummyCheck.php
+++ src/Checks/DummyCheck.php
@@ -2,8 +2,12 @@
 
 namespace Mindtwo\LaravelHealth\Checks;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 use Spatie\Health\Checks\Check;
 use Spatie\Health\Checks\Result;
+use Throwable;
+
 
 class DummyCheck extends Check
 {
@@ -15,4 +19,163 @@ class DummyCheck extends Check
 
         return $result->ok();
     }
+    /**
+     * Get the name of the main system or frame (e.q. Laravel)
+     *
+     * @return string
+     */
+    public function getSystemName(): string
+    {
+        return 'Laravel';
+    }
+
+    /**
+     * Get the version of the main system or frame (e.q. 10.0)
+     *
+     * @return string
+     */
+    public function getSystemVersion(): string
+    {
+        return App::version();
+    }
+
+    /**
+     * Get the php version of the main system of frame (e.q. 8.0)
+     *
+     * @return string
+     */
+
+    public function getPhpVersion(): string
+    {
+        return phpversion();
+    }
+    /**
+     * Get the os name of the main system or frame (e.q. Ubuntu)
+     *
+     * @return string
+     */
+    public function getOsName(): string
+    {
+        return php_uname('s');
+    }
+    /**
+     * Get the os version of the main system or frame (e.q. 20.04 LTS)
+     *
+     * @return string
+     */
+    public function getOsVersion(): string
+    {
+        return php_uname('r');
+    }
+    /**
+     * Get the webserver name of the main system or frame (e.q. Apache)
+     *
+     * @return string
+     */
+    public function getWebserverName(): string
+    {
+        $serverSoftware = explode('/', $_SERVER['SERVER_SOFTWARE']);
+
+        return $serverSoftware[0] ?? '';
+    }
+    /**
+     * Get the webserver version of the main system or frame (e.q. 2.4)
+     *
+     * @return string
+     */
+    public function getWebserverVersion(): string
+    {
+        $serverSoftware = explode('/', $_SERVER['SERVER_SOFTWARE']);
+
+        return $serverSoftware[1] ?? '';
+    }
+
+    /**
+     * Get the database name of the main system or frame (e.q. MySQL)
+     *
+     * @return ?string
+     */
+
+    public function getDatabaseName(): ?string
+    {
+        return config('database.default');
+    }
+
+    /**
+     * Get the database version of the main system or frame (e.q. 5.7)
+     *
+     * @return ?string
+     */
+    public function getDatabaseVersion(): ?string
+    {
+        try {
+            $version = DB::select('SELECT version() AS version');
+
+            return $version[0]->version;
+        } catch (Throwable $error) {
+            return null;
+        }
+    }
+
+    /**
+     * Get the dependencies name of the main system or frame (e.q. Nova)
+     *
+     * @return ?array
+     */
+    public function  getDependenciesInfos(): ?array
+    {
+        return null;
+    }
+    /**
+     * Get the dependencies version of the main system or frame (e.q. 4.1.0)
+     *
+     * @return string
+     */
+
+    public function  getDependenciesVersion(): string
+    {
+        return config('dependencies_version', []);
+    }
+    /**
+     * Get the last update  of the main system or frame (e.q. 2023-10-24T18:00:00Z)
+     *
+     * @return string
+     */
+
+    public function getAdditionalLastUpdate(): string
+    {
+        return now()->toIso8601String();
+    }
+    public function toArray():array
+    {
+        return [
+            'system' => [
+                'type' => $this->getSystemName(),
+                'version' => $this->getSystemVersion(),
+            ],
+            'os' => [
+                'name' => $this->getOsName(),
+                'version' => $this->getOsVersion(),
+            ],
+            'php' => [
+                'version' => $this->getPhpVersion(),
+            ],
+            'webserver' => [
+                'name' => $this->getWebserverName(),
+                'version' => $this->getWebserverVersion(),
+            ],
+            'database' => [
+                'name' => $this->getDatabaseName(),
+                'version' => $this->getDatabaseVersion(),
+            ],
+            'dependencies' => [
+                'name' => $this->getDependenciesInfos(),
+                'version' => $this->getDependenciesInfos(),
+            ],
+            'additional' => [
+                'lastUpdated' => $this->getAdditionalLastUpdate(),
+            ],
+        ];
+    }
+
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,25 +2,88 @@
 
 namespace Mindtwo\LaravelHealth\Tests;
 
+use Mindtwo\LaravelHealth\Checks\DummyCheck;
 use Mindtwo\LaravelHealth\LaravelHealthServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    protected $systemReport;
     protected function setUp(): void
     {
         parent::setUp();
+        $this->systemReport = new DummyCheck();
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             LaravelHealthServiceProvider::class,
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    public function getEnvironmentSetUp($app): void
     {
-        config()->set('database.default', 'testing');
+        //$this->config()->set('database.default', 'testing');
     }
+    public function testSystemName(): void
+    {
+        $this->assertEquals('Laravel', $this->systemReport->getSystemName());
+    }
+
+    public function testSystemVersion(): void
+    {
+        $this->assertEquals('10.30.0', $this->systemReport->getSystemVersion());
+    }
+
+    public function testPhpVersion(): void
+    {
+        $this->assertEquals(phpversion(), $this->systemReport->getPhpVersion());
+    }
+
+    public function testOsName(): void
+    {
+        $this->assertEquals(php_uname('s'), $this->systemReport->getOsName());
+    }
+
+    public function testOsVersion(): void
+    {
+        $this->assertEquals(php_uname('r'), $this->systemReport->getOsVersion());
+    }
+
+    public function testWebserverName(): void
+    {
+        $this->assertEquals($_SERVER['SERVER_SOFTWARE'], $this->systemReport->getWebservername());
+    }
+
+    public function testWebserverVersion(): void
+    {
+        $this->assertEquals('', $this->systemReport->getWebserverVersion());
+    }
+
+    public function testDatabaseName(): void
+    {
+        $this->assertEquals(config('database.default'), $this->systemReport->getDatabaseName());
+    }
+
+    public function testDatabaseVersion(): void
+    {
+        $this->assertEquals( config('database.connections.' . config('database.default') . '.version'), $this->systemReport->getDatabaseVersion());
+    }
+
+    public function testDependenciesName(): void
+    {
+        $this->assertEquals(config('dependencies_name', []), $this->systemReport->getDependenciesName());
+    }
+    public function testDependenciesVersion(): void
+    {
+        $this->assertEquals(config('dependencies_version', []), $this->systemReport->getDependenciesVersion());
+    }
+
+    public function testAdditionalLastUpdate(): void
+    {
+        $this->assertEquals(now()->toIso8601String(), $this->systemReport->getAdditionalLastUpdate());
+    }
+
+
 }
